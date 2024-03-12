9209d0078bed6cbcfb053cc8058848accf1642b3
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-11 21:48:45 -0300

    Primeira versão

---
+++ src/main/java/com/ruanmattos/main/ExercicioForMedia.java
@@ -0,0 +1,29 @@
+package com.ruanmattos.main;
+
+/**
+ * Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste de 3 valores reais, cada um deles uma casa decimal.
+ *
+ * Apresente a média ponderada para cada um destes conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem peso 5.
+ *
+ * Resumo: Você digitará um número para quantidade de número que o programa irá usar para fazer a média
+ *
+ * ENTRADA
+ *
+ * 3
+ *
+ * 6.5
+ *
+ * 4.3
+ *
+ * 6.2
+ *
+ * SAIDA 5.7
+ *
+ * @author Ruan Pereira Mattos
+ */
+public class ExercicioForMedia {
+
+    public static void main(String[] args) {
+
+    }
+}
